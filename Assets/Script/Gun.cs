6ad33a2e304e5d6110310593045e3ac1631add3c
arcamurat <murat.akpek@ug.bilkent.edu.tr>
2024-02-16 23:31:08 +0300

    EggGun 1.1

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class Gun2D : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
- 
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Update mouse position every frame
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * bulletSpeed;
+            // Calculate direction towards mouse position
+            Vector2 direction = (mousePosition - (Vector2)bulletSpawnPoint.position).normalized;
+
+            // Instantiate bullet and set velocity
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
         }
     }
 }
